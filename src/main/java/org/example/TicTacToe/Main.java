ca866736aaf26795fb0cfe605518498bb97c0d48
sumithanvat <skhanvat@gmail.com>
2023-05-23 01:08:56 +0530

    Added:Create board of game

---
+++ src/main/java/org/example/TicTacToe/Main.java
@@ -0,0 +1,12 @@
+package org.example.TicTacToe;
+
+public class Main {
+    public static void main(String[] args) {
+        TicTacToeGame game = new TicTacToeGame();
+
+        // Display the initial board
+        System.out.println("Initial Board:");
+        game.displayBoard();
+
+    }
+}
---
+++ src/main/java/org/example/TicTacToe/TicTacToeGame.java
@@ -0,0 +1,20 @@
+package org.example.TicTacToe;
+
+import java.util.Arrays;
+
+public class TicTacToeGame {
+    private char[] board;
+
+    public TicTacToeGame() {
+        board = new char[10]; // Create a char array of size 10
+        Arrays.fill(board, ' '); // Assign empty spaces to each element of the array
+    }
+
+    public void displayBoard() {
+        System.out.println(" " + board[1] + " | " + board[2] + " | " + board[3] + " "); // Display the board's first row
+        System.out.println("-----------"); // Display the separator line
+        System.out.println(" " + board[4] + " | " + board[5] + " | " + board[6] + " "); // Display the board's second row
+        System.out.println("-----------"); // Display the separator line
+        System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9] + " "); // Display the board's third row
+    }
+}
