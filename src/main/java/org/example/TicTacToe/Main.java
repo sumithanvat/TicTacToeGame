03e53b75bf869add454246d5dc9253f4133ca3ef
sumithanvat <skhanvat@gmail.com>
2023-05-24 00:01:38 +0530

    Added:Move to position in board

--- src/main/java/org/example/TicTacToe/Main.java
+++ src/main/java/org/example/TicTacToe/Main.java
@@ -1,10 +1,19 @@
 package org.example.TicTacToe;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         TicTacToeGame game = new TicTacToeGame();
 
         game.chooseLetter();
+        game.showBoard();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a position to make your move (1-9): ");
+        int position = scanner.nextInt();
+
+        game.makeMove(position);
         game.showBoard();
     }
 }
--- src/main/java/org/example/TicTacToe/TicTacToeGame.java
+++ src/main/java/org/example/TicTacToe/TicTacToeGame.java
@@ -15,9 +15,9 @@ public class TicTacToeGame {
 
     public void displayBoard() {
         System.out.println(" " + board[1] + " | " + board[2] + " | " + board[3] + " "); // Display the board's first row
-        System.out.println("-----------"); // Display separator line
+        System.out.println("-----------"); // Display the separator line
         System.out.println(" " + board[4] + " | " + board[5] + " | " + board[6] + " "); // Display the board's second row
-        System.out.println("-----------"); // Display separator line
+        System.out.println("-----------"); // Display the separator line
         System.out.println(" " + board[7] + " | " + board[8] + " | " + board[9] + " "); // Display the board's third row
     }
 
@@ -25,20 +25,30 @@ public class TicTacToeGame {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Choose a letter (X or O): ");
         char letter = scanner.nextLine().toUpperCase().charAt(0);
-        // Check if the letter is valid
-        if (letter == 'X' || letter == 'O') {
-            // Set the chosen letter as the current player's letter
-            currentPlayer = letter;
+        if (letter == 'X' || letter == 'O') { // Check if the letter is valid
+            currentPlayer = letter; // Set the chosen letter as the current player's letter
             System.out.println("You chose: " + currentPlayer);
         } else {
             System.out.println("Invalid letter choice. Please try again.");
-            // Recursive call to allow the player to choose again
-            chooseLetter();
+            chooseLetter(); // Recursive call to allow the player to choose again
         }
     }
 
     public void showBoard() {
         System.out.println("Current Board:");
         displayBoard(); // Display the current state of the board
+    }
+
+    public void makeMove(int position) {
+        if (position >= 1 && position <= 9) { // Check if the position is within the valid range
+            if (board[position] == ' ') { // Check if the position is empty
+                board[position] = currentPlayer; // Set the current player's letter at the chosen position
+                System.out.println("Move successful.");
+            } else {
+                System.out.println("Position is already occupied. Please choose a different position.");
+            }
+        } else {
+            System.out.println("Invalid position. Please choose a position from 1 to 9.");
+        }
     }
 }
