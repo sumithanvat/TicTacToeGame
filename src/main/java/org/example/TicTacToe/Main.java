15f967e1a99710d857df2d7c43090d10ddfe62ff
sumithanvat <skhanvat@gmail.com>
2023-05-24 00:17:37 +0530

    Added:Do toss who will play first

--- src/main/java/org/example/TicTacToe/Main.java
+++ src/main/java/org/example/TicTacToe/Main.java
@@ -1,5 +1,6 @@
 package org.example.TicTacToe;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -9,11 +10,24 @@ public class Main {
         game.chooseLetter();
         game.showBoard();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a position to make your move (1-9): ");
-        int position = scanner.nextInt();
+        // Toss to determine who plays first
+        Random random = new Random();
+        int tossResult = random.nextInt(2); // Generates 0 or 1 randomly
+        String firstPlayer;
+        if (tossResult == 0) {
+            firstPlayer = "Computer";
+        } else {
+            firstPlayer = "User";
+        }
+        System.out.println("Toss result: " + firstPlayer + " plays first.");
 
-        game.makeMove(position);
-        game.showBoard();
+        // Perform the game moves
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter a position to make your move (1-9): ");
+            int position = scanner.nextInt();
+            game.makeMove(position);
+            game.showBoard();
+        }
     }
 }
